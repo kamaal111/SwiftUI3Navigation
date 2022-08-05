ec6c1e52a20d632558ec3155f572d443e6b417c9
Kamaal <kamaal.f1@gmail.com>
2022-08-05 20:33:03 +0200

    Initial commit

---
+++ SwiftUI3Navigation/SwiftUI3NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3NavigationApp.swift
+//  SwiftUI3Navigation
+//
+//  Created by Kamaal M Farah on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
